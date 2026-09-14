65a47723da3ba586a24f2591d472fba690a2a62b
SAKIL AHAMMED <sakilaha2019@gmail.com>
2026-09-14 21:16:09 +0530

    Create heterogeneous_array.php

---
+++ heterogeneous_array.php
@@ -0,0 +1,7 @@
+<?php
+$student=array("name"=>"ram","age"=>23,"marks"=>85.5,"passed"=>true);
+echo $student["name"]."<br>";
+echo $student["age"]."<br>";
+echo $student["marks"]."<br>";
+echo $student["passed"]."<br>";
+?>
