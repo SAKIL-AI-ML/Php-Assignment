270fd3d1451edef452f85ecfaee3e6d8d25f15a8
SAKIL AHAMMED <sakilaha2019@gmail.com>
2026-09-14 21:10:46 +0530

    Add quiz form for PHP questions
    This file contains a simple HTML form for a PHP quiz application with multiple-choice questions about PHP fundamentals.

---
+++ quize/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Quiz App</title>
+</head>
+<body>
+
+<h2>Simple Quiz App</h2>
+
+<form action="result.php" method="post">
+
+    <p>1. What does PHP stand for?</p>
+    <input type="radio" name="q1" value="Personal Home Page"> Personal Home Page<br>
+    <input type="radio" name="q1" value="Private Home Page"> Private Home Page<br>
+    <input type="radio" name="q1" value="Programming Home Page"> Programming Home Page<br>
+
+    <p>2. Which symbol is used for variables in PHP?</p>
+    <input type="radio" name="q2" value="#"> #<br>
+    <input type="radio" name="q2" value="$"> $<br>
+    <input type="radio" name="q2" value="@"> @<br>
+
+    <p>3. Which function is used to display output in PHP?</p>
+    <input type="radio" name="q3" value="print"> print<br>
+    <input type="radio" name="q3" value="input"> input<br>
+    <input type="radio" name="q3" value="scanf"> scanf<br>
+
+    <p>4. Which extension is used for PHP files?</p>
+    <input type="radio" name="q4" value=".html"> .html<br>
+    <input type="radio" name="q4" value=".php"> .php<br>
+    <input type="radio" name="q4" value=".java"> .java<br><br>
+
+    <input type="submit" value="Submit">
+
+</form>
+
+</body>
+</html>
